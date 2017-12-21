13db90ac45c9b32ea087e9ef72df71c67640e370
dpatrickGithub <dpatr004@gmail.com>
2017-12-20 22:29:05 -0500

    Fill in race feature model

--- DungeonMaster.Models/Race/RaceFeature.cs
+++ DungeonMaster.Models/Race/RaceFeature.cs
@@ -6,5 +6,9 @@ namespace DungeonMaster.Models.Race
 {
     public class RaceFeature : Feature
     {
+        public int Id { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        IEnumerable<StatBonus> StatBonuses { get; set; }
     }
 }
